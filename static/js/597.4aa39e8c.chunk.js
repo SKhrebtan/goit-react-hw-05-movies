9fdf2fdebb319f53b074e9dc6e961e1372f95679
SKhrebtan <131458411+SKhrebtan@users.noreply.github.com>
2023-08-09 15:19:06 +0000

    Deploying to gh-pages from @ SKhrebtan/goit-react-hw-05-movies@8a2abd39532642a15aa8ef2e91e89100e5ccf1fe 🚀

---
+++ static/js/597.4aa39e8c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[597],{597:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Pruvit Cast"})}}}]);
+//# sourceMappingURL=597.4aa39e8c.chunk.js.map
