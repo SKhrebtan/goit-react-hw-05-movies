3976f493697cb99e7b617d1d9fd3434d87318e0c
SKhrebtan <131458411+SKhrebtan@users.noreply.github.com>
2023-08-11 14:38:17 +0000

    Deploying to gh-pages from @ SKhrebtan/goit-react-hw-05-movies@bd583f99155c0130716324b490c4b2862cd19a6b 🚀

--- static/js/431.1c19e455.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[431],{8431:function(e,t,r){r.r(t),r.d(t,{default:function(){return n}});var c=r(184);function n(e){var t=e.cast;return console.log(t),(0,c.jsx)("ul",{children:t.map((function(e){var t=e.id,r=e.name,n=e.character,a=e.profile_path;return(0,c.jsxs)("li",{children:[(0,c.jsx)("img",{src:"https://image.tmdb.org/t/p/w500/".concat(a),alt:r}),(0,c.jsx)("h2",{children:r}),(0,c.jsx)("p",{children:n})]},t)}))})}}}]);
-//# sourceMappingURL=431.1c19e455.chunk.js.map
